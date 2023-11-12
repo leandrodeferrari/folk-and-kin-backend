ee04c9e81d87de61291ad9420d4c8a735c8271fc
Leandro Deferrari <leandro_deferrari@hotmail.com>
2023-11-12 11:54:35 -0300

    FK-2: Creación del Repositorio IUserRepository

---
+++ src/main/java/com/folkandkin/tienda/repository/IUserRepository.java
@@ -0,0 +1,17 @@
+package com.folkandkin.tienda.repository;
+
+import com.folkandkin.tienda.domain.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Clase de acceso a datos para el objeto de dominio Usuario.
+ *
+ * @version v1 12 Nov 2023
+ * @author Leandro Deferrari
+ */
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+}
