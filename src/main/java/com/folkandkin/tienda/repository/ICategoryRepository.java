564e0b9044cc71416387ce431d6cd41a66a1d5d7
Leandro Deferrari <leandro_deferrari@hotmail.com>
2023-11-12 16:26:37 -0300

    FK-6: Creación del Repositorio ICategoryRepository

---
+++ src/main/java/com/folkandkin/tienda/repository/ICategoryRepository.java
@@ -0,0 +1,17 @@
+package com.folkandkin.tienda.repository;
+
+import com.folkandkin.tienda.domain.entity.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Clase de acceso a datos para el objeto de dominio Categoria.
+ *
+ * @version v1 12 Nov 2023
+ * @author Leandro Deferrari
+ */
+
+@Repository
+public interface ICategoryRepository extends JpaRepository<Category, Integer> {
+}
