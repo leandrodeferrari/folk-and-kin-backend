b89decb8d5d3e9d0f448e1875bee1886878acb8c
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2023-11-18 17:51:47 -0300

    FK-14: Endpoint para obtener las categorías (#14)
    FK-14: Agregando endpoint para obtener las categorias de los productos

---
+++ src/main/java/com/folkandkin/tienda/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,35 @@
+package com.folkandkin.tienda.service.impl;
+
+import com.folkandkin.tienda.dto.mapper.ICategoryMapper;
+import com.folkandkin.tienda.dto.response.CategoryResponse;
+import com.folkandkin.tienda.repository.ICategoryRepository;
+import com.folkandkin.tienda.service.ICategoryService;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Clase de objeto de negocio del objeto de dominio Categoria.
+ *
+ * @version v1 18 Nov 2023
+ * @author Leandro Deferrari
+ */
+
+@Service
+public class CategoryServiceImpl implements ICategoryService {
+    private final ICategoryRepository categoryRepository;
+    private final ICategoryMapper categoryMapper;
+
+    public CategoryServiceImpl(ICategoryRepository categoryRepository, ICategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<CategoryResponse> findAll() {
+        return this.categoryMapper.mapToDto(this.categoryRepository.findAll());
+    }
+}
